4828b78980db36e3f171d28f5b21fd83f8b7c6e1
KRaudla <kasparraudla@gmail.com>
2016-10-19 15:59:42 +0300

    commit2

---
+++ src/ylesanded/Time.java
@@ -0,0 +1,18 @@
+package ylesanded;
+import java.util.*;
+import java.util.Date;
+
+/**
+ * Created by kasutaja on 19.10.2016.
+ */
+public class Time {
+
+    public static void main(String[] args) {
+        int hour = 15,minute=41,second=00;
+
+        System.out.println("tt");
+
+    }
+}
+
+
