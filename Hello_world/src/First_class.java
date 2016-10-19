12fd76f34acb659ce3f560c589d375760fd1d5fd
KRaudla <kasparraudla@gmail.com>
2016-10-19 14:46:52 +0300

    First_commit

---
+++ Hello_world/src/First_class.java
@@ -0,0 +1,11 @@
+/**
+ * Created by kasutaja on 19.10.2016.
+ */
+public class First_class {
+    public static void main(String[] args) {
+        System.out.println("The First Program");//First comment
+        int randomNum = 66; //defined integer
+        String firstName= "Kaspar",lastName = "Raudla";//defined strings
+        System.out.println("Created by : "+firstName+" "+lastName);//put the strings together
+    }
+}
