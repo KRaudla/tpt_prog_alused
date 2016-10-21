fd576b96a99da85e13a07febd48084eb93f3341b
KRaudla <kasparraudla@gmail.com>
2016-10-21 08:57:45 +0300

    commit 21.10

--- src/ylesanded/Time.java
+++ src/ylesanded/Time.java
@@ -1,6 +1,7 @@
 package ylesanded;
-import java.util.*;
-import java.util.Date;
+import java.math.RoundingMode;
+import java.time.LocalDateTime;
+
 
 /**
  * Created by kasutaja on 19.10.2016.
@@ -8,9 +9,21 @@ import java.util.Date;
 public class Time {
 
     public static void main(String[] args) {
-        int hour = 15,minute=41,second=00;
 
-        System.out.println("tt");
+        int hour = LocalDateTime.now().getHour();
+        int minute=LocalDateTime.now().getMinute();
+        int second=LocalDateTime.now().getSecond();
+        float secSinMidnight = ((hour*60)*60)+(minute*60)+second;
+        // seconds since midnight
+        System.out.println("Seconds since midnight: " +
+                Math.round(secSinMidnight));
+        float maxSeconds = (24*60)*60;
+        System.out.println("Seconds remaining " +
+                "in the day: "+Math.round(maxSeconds-secSinMidnight));
+        float percent = (secSinMidnight/maxSeconds)*100;
+        System.out.println("The percentage of the day that has passed: "+
+                Math.round(percent)+"%");
+
 
     }
 }
