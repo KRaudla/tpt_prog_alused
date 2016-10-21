63a7873dd30895ce46e15f63e8982224b8bcf5a7
KRaudla <kasparraudla@gmail.com>
2016-10-21 11:15:30 +0300

    commit 21.10_3

---
+++ src/ylesanded/ConvertSeconds.java
@@ -0,0 +1,19 @@
+package ylesanded;
+
+import java.util.Scanner;
+
+/**
+ * Created by kasutaja on 21.10.2016.
+ */
+public class ConvertSeconds {
+    public static void main(String[] args) {
+        Scanner UserInput = new Scanner(System.in);
+        System.out.print("Insert the number of seconds: ");
+        int inputSeconds = UserInput.nextInt();
+        int hours = inputSeconds/3600;
+        int leftSeconds= inputSeconds%3600;
+        int minutes = leftSeconds/60;
+        int  seconds = leftSeconds%60;
+        System.out.println(inputSeconds+" seconds= "+hours + " hour "+minutes+" minutes "+ seconds+ " seconds");
+    }
+}
